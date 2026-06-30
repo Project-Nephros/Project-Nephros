9da21bd99c1bb16b9547c574b291add66795090e
Mikaela <mikaela1685@gmail.com>
2026-06-30 15:49:12 +1000

    added gitignore and alarm system thing

---
+++ Core/Src/AlarmSystem.c
@@ -0,0 +1,24 @@
+// Includes
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "AlarmSystem.h"
+
+// Private Includes
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+void Temp(float temp){
+
+}
+void Pressure(float pressure, int pressureTimer){
+
+}
+
+void Air(int air){
+
+}
+void Normal(float temp, int pressureTimer, int air){
+    
+}
